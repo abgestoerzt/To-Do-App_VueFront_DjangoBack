57d6d2544e4803414ebca93f8da20c498cf4ee98
abgestoerzt <79547235+abgestoerzt@users.noreply.github.com>
2023-02-05 21:40:51 +0100

    items visible in vue

--- frontend/to-do-front/src/components/ToDoApp.vue
+++ frontend/to-do-front/src/components/ToDoApp.vue
@@ -1,7 +1,34 @@
 <template>
   <div class="hello">
     <h1>To-Do-App</h1>
+    <div>
+    <ul>
+      <li v-for="item in response" :key="item.id">{{ item.name }} {{ item.id }}
+        <button @click="deleteItem(item.id)">Delete</button>
+      
+      </li>
+    </ul>
+  
   </div>
+
+
+
+  <div>
+
+    <form @submit.prevent="submitForm">
+      <input type="text" v-model="to_do_name">
+      <button type="submit">Submit</button>
+
+
+    </form>
+   
+  </div>
+
+
+  </div>
+
+
+
 </template>
 
 <script>
@@ -10,18 +37,54 @@ import axios from 'axios';
 
 export default {
   name: 'ToDoApp',
+  data () {
+    return {
+      response: null,
+      to_do_name: null,
+
+    }
+  },
 
   mounted () {
-    axios.get('http://127.0.0.1:8000/todo/'
+    this.fetchData()
+  },
+  methods: {
+    submitForm() {
+      axios.post('http://127.0.0.1:8000/todo/', {
+        name: this.to_do_name
+      })
+        .then(response => {
+          console.log(response.data)
+          this.fetchData()
+        })
+        .catch(error => {
+          console.error(error)
+        })
+    },
+    fetchData(){
+      axios.get('http://127.0.0.1:8000/todo/'
    
       )
       .then(response => {
         this.response = response.data
         console.log(response.data)
+        
       })
       .catch(error => {
         console.error(error)
       })
+
+    },
+    deleteItem(id) {
+      axios.delete(`http://127.0.0.1:8000/todo/${id}`)
+        .then(response => {
+          console.log(response.data)
+          this.fetchData()
+        })
+        .catch(error => {
+          console.error(error)
+        })
+    }
   }
  
 }
@@ -33,11 +96,11 @@ h3 {
   margin: 40px 0 0;
 }
 ul {
-  list-style-type: none;
+  list-style-type: kannada;
   padding: 0;
 }
 li {
-  display: inline-block;
+  display: list-item;
   margin: 0 10px;
 }
 a {
